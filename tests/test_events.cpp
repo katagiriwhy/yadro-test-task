e8ccfb71033d5f83c0af15491137fd8566b9d293
danil.novokhatkiy <karuk.vova2005@mail.ru>
2026-02-06 12:54:34 +0300

    feature: Added README.md and new tests for events

---
+++ tests/test_events.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <sstream>
+#include <limits>
+#include <fstream>
+
+#include "gtest/gtest.h"
+#include "event_handler.hpp"
+
+TEST(EventTest, IOTest) {
+    using namespace novokhatskiy;
+    std::ifstream file(std::string(TEST_DATA_DIR) + "/events.txt");
+    ASSERT_TRUE(file.is_open()) << "Cannot open file";
+
+    auto event = std::make_unique< ClientEvent >();
+    file >> event;
+    EXPECT_EQ(typeid(*event), typeid(ClientEnteredEvent));
+    testing::internal::CaptureStdout();
+    std::cout << *event;
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "08:48 1 client1");
+
+    file >> event;
+    EXPECT_EQ(typeid(*event), typeid(ClientSatEvent));
+    testing::internal::CaptureStdout();
+    std::cout << *event;
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "09:34 2 client2 1");
+
+    file >> event;
+    EXPECT_EQ(typeid(*event), typeid(ClientWaitingEvent));
+    testing::internal::CaptureStdout();
+    std::cout << *event;
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "11:35 3 client3");
+
+    file >> event;
+    EXPECT_EQ(typeid(*event), typeid(ClientLeftEvent));
+    testing::internal::CaptureStdout();
+    std::cout << *event;
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "20:25 4 client2");
+
+    testing::internal::CaptureStdout();
+    std::cout << ErrorEvent(Time({9, 9}), ErrorType::NotOpenYet);
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "09:09 13 NotOpenYet");
+
+    EXPECT_FALSE(file.fail());
+
+    file >> event;
+    while (!file.eof()) {
+        EXPECT_TRUE(file.fail());
+        file.clear();
+        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        file >> event;
+    }
+    file.close();
+}
